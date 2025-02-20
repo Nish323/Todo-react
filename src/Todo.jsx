71535774353650009309689c92b071cb97bf4fd5
Nish323 <doragon.nini@icloud.com>
2025-02-20 17:36:24 +0900

    モックに変更

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,6 +1,16 @@
+import { useState } from 'react';
 import './styles.css';
 
 export const Todo = () => {
+  const [incompleteTosos, setIncompleteTodos] = useState([
+      "TODOです1",
+      "TODOです2"
+    ]);
+  const [completeTosos, setcompleteTodos] = useState([
+    "TODOでした1",
+    "TODOでした2"
+  ]);
+  
   return (
     <>
       <div className='input-area'>
@@ -10,37 +20,29 @@ export const Todo = () => {
       <div className='incomplete-area'>
         <p className='title'>未完了のTODO</p>
         <ul>
-          <li>
-            <div className='list-row'>
-              <p className='todo-item'>TODOです</p>
-              <button>完了</button>
-              <button>削除</button>
-            </div>
-          </li>
-          <li>
-            <div className='list-row'>
-                <p className='todo-item'>TODOです</p>
+          {incompleteTosos.map((todo) => (
+            //DBあるならidとかの一意な要素がいい
+            <li key={todo}>
+              <div className='list-row'>
+                <p className='todo-item'>{todo}</p>
                 <button>完了</button>
                 <button>削除</button>
               </div>
-          </li>
+            </li>
+          ))}
         </ul>
       </div>
       <div className='complete-area'>
         <p className='title'>完了のTODO</p>
         <ul>
-          <li>
-            <div className='list-row'>
-              <p className='todo-item'>TODOでした</p>
-              <button>戻す</button>
-            </div>
-          </li>
-          <li>
-            <div className='list-row'>
-              <p className='todo-item'>TODOでした</p>
-              <button>戻す</button>
-            </div>
-          </li>
+          {completeTosos.map((todo) => (
+            <li ket={todo}>
+              <div className='list-row'>
+                <p className='todo-item'>{todo}</p>
+                <button>戻す</button>
+              </div>
+            </li>
+          ))}
         </ul>
     </div>
     </>
